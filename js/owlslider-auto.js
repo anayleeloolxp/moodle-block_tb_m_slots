6d9e0fa5ac3096aef2c55d038947183e1a7eaad1
Anay Infowind <anay.infowind@gmail.com>
2021-04-21 16:09:38 +0530

    Item numbers

--- js/owlslider-auto.js
+++ js/owlslider-auto.js
@@ -2,7 +2,37 @@ require(["jquery"],function($) {
     
     $(document).ready(function() {
 
-        $('.tb_m_slots').owlCarousel({
+        $('.layout_boxed .tb_m_slots').owlCarousel({
+            loop: true,
+            margin: 10,
+            responsiveClass: true,
+            autoplay: true,
+            dots: false,
+            responsive: {
+                0: {
+                    items: 1,
+                    nav: true
+                },
+                600: {
+                    items: 2,
+                    nav: false
+                },
+                1000: {
+                    items: 2,
+                    nav: true,
+                    dots: false,
+                    margin: 20
+                },
+                1300: {
+                    items: 3,
+                    nav: true,
+                    dots: false,
+                    margin: 20
+                }
+            }
+        });
+
+        $('body:not(.layout_boxed) .tb_m_slots').owlCarousel({
             loop: true,
             margin: 10,
             responsiveClass: true,
@@ -31,5 +61,6 @@ require(["jquery"],function($) {
                 }
             }
         });
+
     });    
 });
